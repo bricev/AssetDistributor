f0d2eabfb865b90894bd353899ffdeca71b069f5
Brice Vercoustre <brice@libcast.com>
2016-03-06 14:41:54 +0100

    Improve example

--- example/index.php
+++ example/index.php
@@ -8,13 +8,20 @@ use Doctrine\Common\Cache\FilesystemCache as Cache;
 use Libcast\AssetDistributor\Owner;
 use Libcast\AssetDistributor\Asset\AssetFactory;
 use Libcast\AssetDistributor\Adapter\AdapterCollection;
-use Libcast\AssetDistributor\YouTube\YouTubeAdapter;
+use Libcast\AssetDistributor\Dailymotion\DailymotionAdapter;
 use Libcast\AssetDistributor\Vimeo\VimeoAdapter;
+use Libcast\AssetDistributor\YouTube\YouTubeAdapter;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 $configPath = 'configuration.ini';
 
+// Optional: set a logger
+$logger = new Logger('distributor');
+$logger->pushHandler(new StreamHandler("$root/example/debug.log", Logger::DEBUG));
+
 // Create an Owner
-$owner = new Owner('messa', new Cache('/tmp'));
+$owner = new Owner('johndoe', new Cache("$root/example/cache"));
 
 // Create an Asset
 $asset = AssetFactory::build(
@@ -27,8 +34,9 @@ $asset->setVisibility('private');
 
 // Create a collection of Adapters that will manage the Asset with Owner credentials
 $adapters = new AdapterCollection;
-$adapters[] = new YouTubeAdapter($owner, $configPath);
-$adapters[] = new VimeoAdapter($owner, $configPath);
+$adapters[] = new YouTubeAdapter($owner, $configPath, $logger);
+$adapters[] = new VimeoAdapter($owner, $configPath, $logger);
+$adapters[] = new DailymotionAdapter($owner, $configPath, $logger);
 
 // Affect the Adapter collection to the Owner
 $owner->setAdapters($adapters);
